9ebb4a325793619bddbdfad60c0373d560648e83
juliantjg <juliantj88@gmail.com>
2023-02-14 18:14:08 +1100

    started security configuration

---
+++ backend/src/main/java/com/homework/backend/config/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package com.homework.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+	
+	private final JwtAuthenticationFilter jwtAuthFilter;
+	private final AuthenticationProvider authenticationProvider;
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+		http
+			.csrf()
+			.disable()
+			.authorizeHttpRequests()
+			.requestMatchers("")
+			.permitAll()
+			.authenticated()
+			.and()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.authenticationProvider(authenticationProvider)
+			.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticatedFilter.class);
+		
+		return http.build();
+	}
+}
